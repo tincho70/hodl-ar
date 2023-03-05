73e5b693c3a7735ec80694b57c288cc910019bfc
Agustin Kassis <webmaster@masize.com>
2023-03-05 13:54:19 -0300

    refactored names

--- components/shared/icons/index.tsx
+++
@@ -1,7 +0,0 @@
-export { default as LoadingDots } from "./LoadingDots";
-export { default as LoadingCircle } from "./LoadingCircle";
-export { default as LoadingSpinner } from "./LoadingSpinner";
-export { default as ExpandingArrow } from "./ExpandingArrow";
-export { default as Github } from "./Github";
-export { default as Twitter } from "./Twitter";
-export { default as Google } from "./Google";
