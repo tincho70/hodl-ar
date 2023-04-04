a1546d2b9b05a00cd6252ac2658c3b929265d59a
Agustin Kassis <webmaster@masize.com>
2023-04-04 00:40:19 -0300

    refactored github lib

--- lib/external/github.ts
+++ lib/external/github.ts
@@ -3,21 +3,23 @@ import { Config } from "lacrypta-links/types/config";
 
 const MAIN_DOMAIN = process.env.MAIN_DOMAIN || "hodl.ar";
 
-export const getUserProfile = async (username: string) => {
-  const res = await fetch(`https://api.github.com/users/${username}`);
-  const data = await res.json();
+const GitHub = {
+  getUserProfile: async (username: string) => {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    const data = await res.json();
 
-  return data;
+    return data;
+  },
+
+  getConfigFromUserRepo: async (username: string): Promise<Config> => {
+    const res = await fetch(
+      `https://raw.githubusercontent.com/${username}/.${MAIN_DOMAIN}/main/config.yml`,
+    );
+
+    const data = await res.text();
+
+    return YAML.parse(data);
+  },
 };
 
-export const getConfigFromUserRepo = async (
-  username: string,
-): Promise<Config> => {
-  const res = await fetch(
-    `https://raw.githubusercontent.com/${username}/.${MAIN_DOMAIN}/main/config.yml`,
-  );
-
-  const data = await res.text();
-
-  return YAML.parse(data);
-};
+export default GitHub;
