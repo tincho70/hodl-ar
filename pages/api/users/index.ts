675878f8043e2d47aec33a9158f4bfc62905db2f
Agustin Kassis <webmaster@masize.com>
2023-04-05 18:46:10 -0300

    list users

---
+++ pages/api/users/index.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Prisma } from "@prisma/client";
+
+// Create Prisma Client
+const prisma = new PrismaClient();
+
+// export the default function
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const _users = await prisma.user.findMany({});
+
+  const users = _users.map((user) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+  });
+
+  console.info("TODY: users");
+  console.dir(users);
+  res.status(200).json(users);
+}
