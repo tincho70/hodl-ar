3db41e08f34ff450ffcab8e64e5e60a6afe3ced3
Agustin Kassis <webmaster@masize.com>
2023-04-28 20:13:58 -0300

    wip for users

--- pages/api/wallets/create.ts
+++ pages/api/wallets/create.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { ResponseDataType } from "types/request";
-import { User, LNBits as LNBitsConfig } from ".prisma/client";
-import { Prisma } from "@prisma/client";
 
 const MAIN_DOMAIN = process.env.MAIN_DOMAIN || "hodl.ar";
 const LNBITS_ENDPOINT =
@@ -17,7 +15,6 @@ const createUserRequestSchema = z.object({
 });
 
 // External Libraries
-import GitHub from "@/lib/external/github";
 import LNBits from "@/lib/external/lnbits";
 import NextCors from "nextjs-cors";
 
@@ -55,49 +52,70 @@ export default async function handler(
   // Get body data
   const { username, provider } = result.data;
 
-  // TODO
-  const github = username;
-
   // Wrap any errors in a try/catch
   try {
     if (provider !== "github") {
       throw new Error("Only GitHub is supported");
     }
 
-    const githubProfile = await GitHub.getUserProfile(github);
-    const userConfig = await GitHub.getConfigFromUserRepo(github);
+    // Create LnBits User
+    const lnbitsUser = await LNBits.createUser(user.id);
 
-    const { name, bio, twitter_username, email } = githubProfile;
+    // Create Link
+    const link = await LNBits.createLNURLp(lnbitsUser);
 
-    if (!userConfig?.username) {
-      throw new Error("No username provided");
-    }
+    console.info("LINK");
+    console.dir(link);
 
-    // TODO: Need to handle validation
-    const user: User = {
-      id: Math.random().toString(36).substring(2, 15),
-      // id: userConfig.username,
-      name,
-      bio,
-      twitter: twitter_username,
-      email,
-      github,
-      nostr: userConfig.nostr?.npub || null,
-      discord: null,
-      lud06: null,
-      relayIDs: [],
-    };
-
-    // Create User on Database
-    await createUser(user);
-
-    console.info("** Created User");
-    console.dir(user);
+    await Promise.all([
+      // Add LnBits User to Database
+      await prisma.lNBits.create({
+        data: {
+          id: lnbitsUser.id,
+          userId: user.id,
+          adminKey: lnbitsUser.wallets[0].inkey,
+        },
+      }),
+      // Add LNURLp to database
+      await prisma.lNURL.create({
+        data: {
+          tag: "payRequest",
+          callback: `${LNBITS_ENDPOINT}/lnurlp/api/v1/lnurl/cb/${link.id}`,
+          minSendable: link.min * 1000,
+          maxSendable: link.max * 1000,
+          metadata: `[[\"text/plain\", \"${link.description}\"]]`,
+          commentAllowed: link.comment_chars,
+          id: link.id,
+          userId: user.id,
+        },
+      }),
+      // Set LNURLp to user
+      await prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          lud06: link.lnurl,
+        },
+      }),
+    ]);
 
     // Success
     res.status(200).json({
       success: true,
-      data: user,
+      data: {
+        username: user.id,
+        handle: `${user.id}@${MAIN_DOMAIN}`,
+        lnAddress: link.lnurl,
+        lnbitUser: lnbitsUser.id,
+        endpoint: LNBITS_ENDPOINT,
+        walletUrl: `${LNBITS_ENDPOINT}/wallet?usr=${lnbitsUser.id}`,
+        lndhub: {
+          login: "admin",
+          password: lnbitsUser.wallets[0].inkey,
+          url: `${LNBITS_ENDPOINT}/lndhub/ext`,
+        },
+      },
     });
   } catch (e: any) {
     console.dir(e);
@@ -105,22 +123,3 @@ export default async function handler(
     return;
   }
 }
-
-const createUser = async (data: Prisma.UserCreateInput) => {
-  const currentDate = new Date();
-  const validUntil = new Date(currentDate.getTime() + 15 * 60 * 1000);
-
-  const newUser = await prisma.user.create({
-    data: {
-      ...data,
-      otToken: {
-        create: {
-          validUntil,
-        },
-      },
-    },
-  });
-
-  console.dir(newUser);
-  return newUser;
-};
